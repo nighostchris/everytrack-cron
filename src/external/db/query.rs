2ae1c9d105f959abb18daa04dfe0f903f4ad6731
Chris <chrisliupascal@gmail.com>
2024-03-12 09:22:39 +0000

    Introduce query submodule inside db to centralize all database queries + Initial draft of account and account stock holding snapshots query functions

---
+++ src/external/db/query.rs
@@ -0,0 +1,2 @@
+pub mod account;
+pub mod account_stock;
--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,7 @@ mod logger;
 mod server;
 
 use config::load_env_vars;
-use external::db;
+// use external::db;
 
 #[tokio::main]
 async fn main() {
@@ -13,7 +13,13 @@ async fn main() {
   // Initialize logger
   logger::init();
   // Create a new database client
-  let db_client = db::client::init().await;
+  // let db_client = db::client::init().await;
   // Initialize web server
-  server::init(db_client.clone()).await;
+  // server::init(db_client.clone()).await;
+  // let account_balance_snapshots =
+  //   db::query::account::get_account_balance_snapshots(&db_client).await;
+  // println!("{account_balance_snapshots:?}");
+  // let account_stock_holding_balance_snapshots =
+  //   db::query::account_stock::get_account_stock_holding_balance_snapshots(&db_client).await;
+  // println!("{account_stock_holding_balance_snapshots:?}");
 }
